e84e8611c4eb3d45f886b8ea54e44cf720c10c58
andresagdt515 <83793367+andresagdt515@users.noreply.github.com>
2021-05-09 20:41:16 +0200

    666

--- Scripts/2Dnetwork.js
+++ Scripts/2Dnetwork.js
@@ -1,55 +1,55 @@
-var canvas = d3.select("#network"),
+var canvas = d3.select("#network"), //generate canvas in previously stated network
         width = canvas.attr("width"),
         height = canvas.attr("height"),
-        ctx = canvas.node().getContext("2d"),
-        r = 10,
+        ctx = canvas.node().getContext("2d"), // generate context for our canvas
+        r = 10, //radius length
         color = d3.scaleOrdinal(d3.schemeCategory20),
-        simulation = d3.forceSimulation()
-            .force("x", d3.forceX(width/2))
-            .force("y", d3.forceY(height/2))
-            .force("collide", d3.forceCollide(r+1))
-            .force("charge", d3.forceManyBody()
+        simulation = d3.forceSimulation() // create simulation forces
+            .force("x", d3.forceX(width/2)) // force to bring to the middle of page, in x axis
+            .force("y", d3.forceY(height/2)) // force to bring to the middle of page, in y axis
+            .force("collide", d3.forceCollide(r+1)) // force to avoid nodes to collide
+            .force("charge", d3.forceManyBody() // force FAAALTAAA
             .strength(-700))
             .force("link", d3.forceLink()
             .id(function (d) { return d.id; }));
         
         
-        d3.json("../Task2_Web_Development/Data/data2D.json", function (err, graph) {
+        d3.json("../Task2_Web_Development/Data/data2D.json", function (err, graph) { // load data
         if (err) throw err;
         
-        simulation.nodes(graph.nodes);
-        simulation.force("link")
+        simulation.nodes(graph.nodes); // create node elements in simulation
+        simulation.force("link") // create edge streuctures in simulation
             .links(graph.edges);
         simulation.on("tick", update);
         
         canvas
-            .call(d3.drag()
+            .call(d3.drag() // dragging of nodes
                 .container(canvas.node())
                 .subject(dragsubject)
                 .on("start", dragstarted)
                 .on("drag", dragged)
                 .on("end", dragended));
         
-        function update() {
-            ctx.clearRect(0, 0, width, height);
+        function update() { 
+            ctx.clearRect(0, 0, width, height); // function that will clear the whole canvas
         
-            ctx.beginPath();
-            ctx.globalAlpha = 1;
-            ctx.strokeStyle = "#000000";
-            graph.edges.forEach(drawLink);
-            ctx.stroke();
+            ctx.beginPath(); // let's begin the drawing of nodes and edges
+            ctx.globalAlpha = 1; 
+            ctx.strokeStyle = "#000000"; // black links
+            graph.edges.forEach(drawLink); // designs edges with arrows using drawLink
+            ctx.stroke(); //draws edges with arrows
         
             ctx.globalAlpha = 1.0;
-            graph.nodes.forEach(drawNode);
+            graph.nodes.forEach(drawNode);// design and draw nodes using drawNode
         }
         
         function dragsubject() {
-            return simulation.find(d3.event.x, d3.event.y);
+            return simulation.find(d3.event.x, d3.event.y); // display  node information in JavaScript console when clicking on a node
         }
         
         });
         
-        function dragstarted() {
+        function dragstarted() { // The following 3 functions are in charge of the dragging effect. They control the start of the dragging, its event and its end
             if (!d3.event.active) simulation.alphaTarget(0.3).restart();
                 d3.event.subject.fx = d3.event.subject.x;
                 d3.event.subject.fy = d3.event.subject.y;
@@ -67,24 +67,28 @@ var canvas = d3.select("#network"),
             d3.event.subject.fy = null;
         }
         
-        function drawNode(d) {
+        function drawNode(d) { // function to design and draw nodes, receives an element d
             ctx.beginPath();
 
-            var c = d.color.slice(2);
+            var c = d.color.slice(2); // since color code from data needs to be a 6 digit code,
+                                      // without including the initial 0x. For that we first remove 
+                                      //this 0x creating a substring
         
-            while (c.length < 6){
+            while (c.length < 6){ // then we check if the substring has less than 6 digit length. 
+                                 // If so then we add a 0 at the beginning.
                 c = "0" + c;   
             }
             
-            c = "#" + c
+            c = "#" + c // Finally, we add the # at the beginning of c to have our final hex code, ready for drawing the node.
             
-            ctx.fillStyle = c;
-            ctx.moveTo(d.x, d.y);
-            ctx.arc(d.x, d.y, r, 0, Math.PI*2);
-            ctx.fill();
+            ctx.fillStyle = c; // set the color to node
+            ctx.moveTo(d.x, d.y); // move cursor to (x,y)
+            ctx.arc(d.x, d.y, r, 0, Math.PI*2); // draw circle centered in (x,y)
+            ctx.fill(); // fillcircle with previously selected color
         }
 
-        function findCircleLineIntersections(r, h, k, m, n) {
+        function findCircleLineIntersections(r, h, k, m, n) { // This function returns the x coordinates of points P0=(x0,y0) and P1=(x1,y1), 
+                                                              // that are the 2 intersections of a line crossing a circle
             var a = 1 + (m)**2;
             var b = -h * 2 + (m * (n - k)) * 2;
             var c = h**2 + (n - k)**2 - (r)**2;
@@ -104,7 +108,10 @@ var canvas = d3.select("#network"),
             }
             return [];
         }
-        function canvas_arrow(context, fromx, fromy, tox, toy) {
+        function canvas_arrow(context, fromx, fromy, tox, toy) { // This function creates an arrow at the end of the link. 
+                                                                 // But since nodes are opaque, the arrow overlaps with the node and can't be well distinguished.
+                                                                 //This is why I implement the function findCircleLineIntersections, to have the surface-node-point
+                                                                 //  where the edge crosses the node and then setting the tox and toy coordinates of the arrow  in that point.
             var headlen = 10; // length of head in pixels
             var dx = tox - fromx;
             var dy = toy - fromy;
@@ -116,20 +123,21 @@ var canvas = d3.select("#network"),
             context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
           }
           
-        function drawLink(l) {// function to draw links, receives a link l
+        function drawLink(l) { // function to design links with arrows, receives an element l
             var m = (l.target.y - l.source.y)/(l.target.x - l.source.x);
             var n = l.target.y - l.target.x * m;
             var xf = 0;
             
-            pos = findCircleLineIntersections(r, l.target.x, l.target.y, m, n);
+            pos = findCircleLineIntersections(r, l.target.x, l.target.y, m, n); // pos stores x0 and x1
 
-            if (Math.abs(l.source.x - pos[0]) < Math.abs(l.source.x - pos[1])){
+            if (Math.abs(l.source.x - pos[0]) < Math.abs(l.source.x - pos[1])){ // We need to choose either P0 or P1 to tell the arrow to draw.
+                                                                                // We will choose the one that is nearer to the source point.
                 xf = pos[0];
             }else{
                 xf = pos[1]
             }
-            var yf = m*(xf-l.target.x) + l.target.y;
+            var yf = m*(xf-l.target.x) + l.target.y; // and then, once the x coordinate is selected, we calculate the y coordinate from line equation
 
-            canvas_arrow(ctx, l.source.x, l.source.y, xf, yf)
+            canvas_arrow(ctx, l.source.x, l.source.y, xf, yf) // And finally we design edge + arrows
         };
         
--- Scripts/3Dnetwork.js
+++ Scripts/3Dnetwork.js
@@ -1,11 +1,11 @@
 
-            jgraph.create('.graph', {directed: true});
-            $.getJSON('../Task2_Web_Development/Data/data3D.json', function (graph) {
+            jgraph.create('.graph', {directed: true}); //draw 3D directed network using jgraph's create function
+            $.getJSON('../Task2_Web_Development/Data/data3D.json', function (graph) { // loading data
                 jgraph.draw(graph);
             });
 
-            $('#toon').click(function () { jgraph.setShader('toon'); });
-            $('#basic').click(function () { jgraph.setShader('basic'); });
-            $('#phong').click(function () { jgraph.setShader('phong'); });
-            $('#lambert').click(function () { jgraph.setShader('lambert'); });
+            // $('#toon').click(function () { jgraph.setShader('toon'); });
+            // $('#basic').click(function () { jgraph.setShader('basic'); });
+            // $('#phong').click(function () { jgraph.setShader('phong'); });
+            // $('#lambert').click(function () { jgraph.setShader('lambert'); });
         
